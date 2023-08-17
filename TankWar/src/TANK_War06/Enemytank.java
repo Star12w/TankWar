19c846df5fc414a1d4f65feaa187f3cbab4e25d0
Star12w <zhaoqich@gmail.com>
2023-08-17 22:42:50 +0800

    Update Enemytank.java

--- TankWar/src/TANK_War06/Enemytank.java
+++ TankWar/src/TANK_War06/Enemytank.java
@@ -11,23 +11,58 @@ public class Enemytank extends TANK implements Runnable{
 
     @Override
     public void run() {
+         setSpeed(2);
         while (true) {
-            int direct = (int) (Math.random() * 4);
-            switch (direct){
+            switch (getDirect()){
                 case 0 : //表示向上
-                    setY(getY());
-                    setDirect(0);
-                case 1 ://表示向右转
-                    setX(getX()+1);
-                    setDirect(1);
-                case 2 ://表示向下转
-                    setY(getY()+1);
-                    setDirect(2);
-                case 3 ://表示向左转
-                    setX(getX()-1);
-                    setDirect(3);
-        }
+                    for (int i = 0 ; i <30 ; i++) {
+                        W();
+                        try {   //每走一步有50毫秒的延迟
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
 
-    }
+                case 1 ://表示向右转
+                    for (int i = 0 ; i <30 ; i++) {
+                        D();
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+
+                case 2 ://表示向下转
+                    for (int i = 0 ; i <30 ; i++) {
+                        S();
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+
+                case 3 ://表示向左转
+                    for (int i = 0 ; i <30 ; i++) {
+                        A();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+
+            }
+            setDirect((int) (Math.random() * 4));
+            if(!islive){
+                break;
+            }
+        }
     }
 }
